ae50df7a8d776a56180efeaf44651bc5a0bfb1f5
habs <brenohabib2013@gmail.com>
2026-01-19 15:43:10 -0300

    - Added UI for Data

--- autouc/web/__init__.py
+++ autouc/web/__init__.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_GET, require_POST
 
-from ..devices import TPLinkExFirmware, TPLinkExPreset
+from ..devices import TPLinkExFirmware, TPLinkExPreset, DataFirmware, DataPreset
 
 
 DEVICE_IP = "192.168.0.1"
@@ -51,6 +51,30 @@ def use_preset(request):
     try:
         with sync_playwright() as playwright:
             TPLinkExPreset.run(playwright)
+        return JsonResponse({"ok": True})
+    except Exception:
+        return JsonResponse({"ok": False}, status=500)
+
+
+@require_POST
+def data_update_firmware(request):
+    from playwright.sync_api import sync_playwright
+
+    try:
+        with sync_playwright() as playwright:
+            DataFirmware.run(playwright)
+        return JsonResponse({"ok": True})
+    except Exception:
+        return JsonResponse({"ok": False}, status=500)
+
+
+@require_POST
+def data_use_preset(request):
+    from playwright.sync_api import sync_playwright
+
+    try:
+        with sync_playwright() as playwright:
+            DataPreset.run(playwright)
         return JsonResponse({"ok": True})
     except Exception:
         return JsonResponse({"ok": False}, status=500)
